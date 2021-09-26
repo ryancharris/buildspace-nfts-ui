e166112a1be2fd29c85a174feee171b7e273b216
Ryan Harris <harrisryan1@gmail.com>
2021-09-26 17:34:23 -0400

    add loading states

--- src/App.js
+++ src/App.js
@@ -1,30 +1,179 @@
-import './styles/App.css';
-import twitterLogo from './assets/twitter-logo.svg';
-import React from "react";
+import "./styles/App.css";
+import twitterLogo from "./assets/twitter-logo.svg";
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import myEpicNft from "./utils/MyEpicNFT.json";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "ryan_c_harris";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = '';
+const OPENSEA_LINK = "";
 const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
-  // Render Methods
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const [loading, setLoading] = useState(false);
+  const [connecting, setConnecting] = useState(false);
+
+  useEffect(() => {
+    checkIfWalletConnected();
+  }, []);
+
+  const checkIfWalletConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Connect your MetaMask wallet!");
+    } else {
+      console.log("You are connected to MetaMask", ethereum);
+    }
+
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+      setupEventListener();
+    } else {
+      console.log("No authorized account found");
+    }
+  };
+
+  const connectWallet = async () => {
+    setConnecting(true);
+
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+      setupEventListener();
+      setConnecting(false);
+    } catch (err) {
+      console.log(err);
+      setConnecting(false);
+    }
+  };
+
+  const askContractToMintNFT = async () => {
+    const CONTRACT_ADDRESS = "0xDB24bE6Fb6c5e4c6F3C165663203b02bBDA70B59";
+    try {
+      setLoading(true);
+
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNft.abi,
+          signer
+        );
+
+        console.log("Going to pop wallet now to pay gas...");
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+
+        console.log("Mining...please wait.");
+        await nftTxn.wait();
+
+        console.log(
+          `Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`
+        );
+        setLoading(false);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const setupEventListener = async () => {
+    const CONTRACT_ADDRESS = "0xDB24bE6Fb6c5e4c6F3C165663203b02bBDA70B59";
+
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNft.abi,
+          signer
+        );
+
+        // THIS IS THE MAGIC SAUCE.
+        // This will essentially "capture" our event when our contract throws it.
+        // If you're familiar with webhooks, it's very similar to that!
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber());
+          alert(
+            `Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          );
+        });
+
+        console.log("Setup event listener!");
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
       Connect to Wallet
     </button>
   );
+
+  const renderMintUi = () => {
+    return (
+      <button
+        onClick={askContractToMintNFT}
+        className="cta-button connect-wallet-button"
+      >
+        Mint NFT
+      </button>
+    );
+  };
 
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">My NFT Collection</p>
-          <p className="sub-text">
-            Each unique. Each beautiful. Discover your NFT today.
-          </p>
-          {renderNotConnectedContainer()}
+          <p className="header gradient-text">What Will Happen NFTs</p>
+          <p className="sub-text">Tech predictions. General sillyness.</p>
+
+          {!loading && connecting && (
+            <p className="sub-text sub-text--small">Connecting...</p>
+          )}
+          {loading && !connecting && (
+            <p className="sub-text sub-text--small">Loading...</p>
+          )}
+          {!loading &&
+            !connecting &&
+            (currentAccount === ""
+              ? renderNotConnectedContainer()
+              : renderMintUi())}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
@@ -33,7 +182,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>

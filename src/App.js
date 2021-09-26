e32c59767b996ebdefb6a86ddd6c614fb42df1d7
Ryan Harris <harrisryan1@gmail.com>
2021-09-26 18:19:47 -0400

    styles

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ import myEpicNft from "./utils/MyEpicNFT.json";
 // Constants
 const TWITTER_HANDLE = "ryan_c_harris";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = "";
+const OPENSEA_LINK = "https://testnets.opensea.io/assets";
 const TOTAL_MINT_COUNT = 50;
+const CONTRACT_ADDRESS = "0xDB24bE6Fb6c5e4c6F3C165663203b02bBDA70B59";
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
-
   const [loading, setLoading] = useState(false);
   const [connecting, setConnecting] = useState(false);
+  const [id, setId] = useState(null);
 
   useEffect(() => {
     checkIfWalletConnected();
@@ -66,7 +67,6 @@ const App = () => {
   };
 
   const askContractToMintNFT = async () => {
-    const CONTRACT_ADDRESS = "0xDB24bE6Fb6c5e4c6F3C165663203b02bBDA70B59";
     try {
       setLoading(true);
 
@@ -97,12 +97,11 @@ const App = () => {
       }
     } catch (error) {
       console.log(error);
+      setLoading(false);
     }
   };
 
   const setupEventListener = async () => {
-    const CONTRACT_ADDRESS = "0xDB24bE6Fb6c5e4c6F3C165663203b02bBDA70B59";
-
     // Most of this looks the same as our function askContractToMintNft
     try {
       const { ethereum } = window;
@@ -122,9 +121,8 @@ const App = () => {
         // If you're familiar with webhooks, it's very similar to that!
         connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
           console.log(from, tokenId.toNumber());
-          alert(
-            `Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
-          );
+
+          setId(tokenId.toNumber());
         });
 
         console.log("Setup event listener!");
@@ -162,18 +160,32 @@ const App = () => {
         <div className="header-container">
           <p className="header gradient-text">What Will Happen NFTs</p>
           <p className="sub-text">Tech predictions. General sillyness.</p>
-
+          {!loading &&
+            !connecting &&
+            (currentAccount === ""
+              ? renderNotConnectedContainer()
+              : renderMintUi())}
           {!loading && connecting && (
             <p className="sub-text sub-text--small">Connecting...</p>
           )}
           {loading && !connecting && (
             <p className="sub-text sub-text--small">Loading...</p>
           )}
-          {!loading &&
-            !connecting &&
-            (currentAccount === ""
-              ? renderNotConnectedContainer()
-              : renderMintUi())}
+        </div>
+        <div className="content-container">
+          {id && (
+            <div>
+              <p className="sub-text">Last minted:</p>
+              <div className="last-minted">
+                <p className="sub-text sub-text--small">#{id}</p>
+                <p className="sub-text sub-text--small">
+                  <a href={`${OPENSEA_LINK}/${CONTRACT_ADDRESS}/${id}`}>
+                    {`${CONTRACT_ADDRESS}/${id}`}
+                  </a>
+                </p>
+              </div>
+            </div>
+          )}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
